6045d56de264685b31a2e6f3d8e71211ab971572
juan <jortegah79@gmail.com>
2022-02-16 22:49:09 +0100

    probando a subirlo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+class EditorDeMarcado extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    }
+    this.cambioTexto=this.cambioTexto.bind(this);
+  }
+  cambioTexto(event){
+    this.setState({
+      input: event.target.value
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1 className="display-4 text-center text-light bg-success border-bottom">Editor de marcado de J.Ortega</h1>
+        <div className="caja editor">
+          <div className="barra">
+            <h3>Editor</h3>
+            <button><i className="bi bi-x-square-fill h4 "></i></button>
+          </div>
+          <div className="textos">
+            <textarea value={this.state.input} onChange={this.cambioTexto} className="txt"></textarea>
+          </div>
+        </div>
+        <div className="caja preview">
+          <div className="barra">
+            <h3>Preview</h3>
+           <button><i className="bi bi-x-square-fill h4 "></i></button>
+          </div>
+          <div className="textos">
+            <textarea value={this.state.input}className="txt" disabled></textarea>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <EditorDeMarcado />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
